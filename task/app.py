6a21bfc85bd62e030d9aed6b7e08fdfd27bb7e12
ackmedtijani <tijanioboiriendev@gmail.com>
2023-09-08 10:27:12 +0100

    Modified the utc_time output

--- task/app.py
+++ task/app.py
@@ -15,7 +15,7 @@ async def endpoint(slack_name :str , track:str):
     daytime = datetime.now()
     
     return {"slack" : slack_name,
-             "utc_time" : daytime.astimezone(tz = pytz.UTC),
+             "utc_time" : daytime.astimezone(tz = pytz.UTC).strftime('%Y-%m-%dT%H:%M:%SZ'),
             "current_day" : daytime.strftime("%A"),
             "track": track,
             "github_file_url": "https://github.com/tephlonee/hngx/blob/main/task/app.py",
