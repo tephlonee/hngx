3450f71d344de46b83c200e030048527de650153
ackmedtijani <tijanioboiriendev@gmail.com>
2023-09-08 10:19:21 +0100

    Rearrange the order of the url endpoint

--- task/app.py
+++ task/app.py
@@ -15,8 +15,8 @@ async def endpoint(slack_name :str , track:str):
     daytime = datetime.now()
     
     return {"slack" : slack_name,
+             "utc_time" : daytime.astimezone(tz = pytz.UTC),
             "current_day" : daytime.strftime("%A"),
-            "utc_time" : daytime.astimezone(tz = pytz.UTC),
             "track": track,
             "github_file_url": "https://github.com/tephlonee/hngx/blob/main/task/app.py",
              "github_repo_url": "https://github.com/tephlonee/hngx",
